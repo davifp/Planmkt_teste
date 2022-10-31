0f92ee1ddef1ac88950b846d9ca921cbd8200188
Davi Pavone <davipavone@gmail.com>
2022-10-30 22:07:39 -0300

    fix group cards

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -4,23 +4,30 @@ import styles from "./index.module.scss";
 
 export const Contact: React.FC = () => {
   return (
-    <div className={styles.contactContainer}>
-      <div className={styles.wrapper}>
-        <div className={styles.title}>
-          <h1>/Contato</h1>
-          <span>05.</span>
+    <div className={styles.container}>
+      <div className={styles.contactContainer}>
+        <div className={styles.wrapper}>
+          <div className={styles.title}>
+            <h1>/Contato</h1>
+            <span>05.</span>
+          </div>
+          <p>
+            Gostou de saber mais sobre as inovações tecnológicas e também quer
+            oferecer novas experiências aos seus clientes? Então entre em
+            contato e marque um encontro real conosco para conversarmos sobre as
+            infinitas possibilidades do mundo virtual.
+          </p>
+          <Form />
         </div>
-        <p>
-          Gostou de saber mais sobre as inovações tecnológicas e também quer
-          oferecer novas experiências aos seus clientes? Então entre em contato
-          e marque um encontro real conosco para conversarmos sobre as infinitas
-          possibilidades do mundo virtual.
-        </p>
-        <Form />
+        <div className={styles.cardWrapper}>
+          <GroupCard />
+          <GroupCard />
+          <GroupCard />
+          <GroupCard />
+          <GroupCard />
+          <GroupCard />
+        </div>
       </div>
-      <GroupCard />
-      <GroupCard />
-      <GroupCard />
     </div>
   );
 };
