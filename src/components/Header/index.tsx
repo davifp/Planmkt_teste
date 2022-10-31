c1b40aa7378981e7f1d9334a1a62ba6388d6a74d
Davi Pavone <davipavone@gmail.com>
2022-10-31 00:03:46 -0300

    fix responsive menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,14 +3,52 @@ import { Navigation } from "../Navigation";
 import Image from "next/image";
 import { FiArrowDownRight } from "react-icons/fi";
 import logo from "../../../public/logo_plan.png";
+import { useState } from "react";
 
 export const Header: React.FC = () => {
+  const [show, setShow] = useState(false);
+
+  function handleMenuClick() {
+    setShow(!show);
+  }
+
   return (
     <header className={styles.headerContainer}>
       <div>
         <Image src={logo} alt="logo image" className={styles.logo} />
       </div>
-      <Navigation />
+      <div className={show ? styles.menuSectionOn : styles.menuSection}>
+        <div onClick={handleMenuClick} className={styles.menuToggle}>
+          <div className={styles.one}></div>
+          <div className={styles.two}></div>
+          <div className={styles.three}></div>
+        </div>
+        <nav>
+          <ul>
+            <li className={styles.anchorContainer}>
+              <a href="">/QUEM SOMOS</a>
+              <span>01.</span>
+            </li>
+            <li className={styles.anchorContainer}>
+              <a href="">/O QUE FAZEMOS</a>
+              <span>02.</span>
+            </li>
+            <li className={styles.anchorContainer}>
+              <a href="">/PLAN NEWS</a>
+              <span>03.</span>
+            </li>
+            <li className={styles.anchorContainer}>
+              <a href="">/NOSSOS CLIENTES</a>
+              <span>04.</span>
+            </li>
+            <li className={styles.anchorContainer}>
+              <a href="">/CONTATO</a>
+              <span>05.</span>
+            </li>
+          </ul>
+        </nav>
+        <Navigation />
+      </div>
       <footer className={styles.footer}>
         <div className={styles.textContainer}>
           <p>Que tal inovar com a gente?</p>
